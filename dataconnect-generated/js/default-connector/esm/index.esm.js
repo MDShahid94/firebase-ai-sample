b3838d93afeacfe693fc320579ac43190a1232a4
MDShahid94 <39449684+MDShahid94@users.noreply.github.com>
2025-03-27 22:50:57 +0530

    hh

---
+++ dataconnect-generated/js/default-connector/esm/index.esm.js
@@ -0,0 +1,7 @@
+
+export const connectorConfig = {
+  connector: 'default',
+  service: 'firebase-ai-sample',
+  location: 'us-central1'
+};
+
---
+++ dataconnect-generated/js/default-connector/index.cjs.js
@@ -0,0 +1,8 @@
+
+const connectorConfig = {
+  connector: 'default',
+  service: 'firebase-ai-sample',
+  location: 'us-central1'
+};
+exports.connectorConfig = connectorConfig;
+
